c8d1e9c25144ac7ce2ffecdc842cd335498ac7ff
Vjacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2023-05-25 14:46:57 +0300

    The project has been completed.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>
+    <h1>{{ $t("home.header") }}</h1>
+    <p>{{ $t("home.created_by") }}</p>
+    <p>{{ $t("home.num_visits") }}</p>
+    <p>Lorem ipsum dolor sit amet.</p>
+  </main>
+</template>
